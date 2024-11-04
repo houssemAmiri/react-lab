280f67896b5724dcd98ebd236a9637443a57e4dd
Houssem Amiri <houssem.amiri@proton.me>
2024-11-04 09:26:03 -0500

    fix: eslint problems

--- eslint.config.js
+++ eslint.config.js
@@ -3,38 +3,26 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import jsxA11y from "eslint-plugin-jsx-a11y";
+import react from 'eslint-plugin-react'
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    settings: { react: { version: "18.3" } },
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecke,
-      "eslint:recommended",
-      react.configs.recommended,
-      jsxA11y.configs.recommended,
-    ],
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    settings: { react: { version: '18.3' } },
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       react,
-      "jsx-a11y": jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -42,10 +30,8 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
-      // Enable its recommended rules
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      "jsx-a11y/alt-text": "error",
+      ...react.configs['jsx-runtime'].rules,
     },
   }
 );
